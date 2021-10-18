b0b19d037c1e53330497aacc7493cb8678fdd970
Aabhaas Jain <aabhaas.jain@dal.ca>
2021-10-18 00:16:07 -0300

    initial code setup

---
+++ src/main/java/com/bankingmanagement/bankingmangement/BankingMangementApplication.java
@@ -0,0 +1,13 @@
+package com.bankingmanagement.bankingmangement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingMangementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingMangementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bankingmanagement/bankingmangement/BankingMangementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankingmanagement.bankingmangement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingMangementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
