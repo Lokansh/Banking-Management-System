f79ebc5c86e5a3ac6290c7feb6d82b2dcbab16f0
Aabhaas Jain <aabhaas.jain@dal.ca>
2021-11-17 06:14:16 -0400

    Feature 7: DB connectiveity qeury implementation in service

--- src/main/java/com/bankingmanagement/bankingmanagement/employeeDashboard/serviceImplementation/EmployeeDetailsServiceImpl.java
+++ src/main/java/com/bankingmanagement/bankingmanagement/employeeDashboard/serviceImplementation/EmployeeDetailsServiceImpl.java
@@ -1,5 +1,6 @@
 package com.bankingmanagement.bankingmanagement.employeeDashboard.serviceImplementation;
 
+import static com.bankingmanagement.bankingmanagement.authentication.database.LoginConstants.LOGIN_PASSWORD;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -8,14 +9,43 @@ import java.sql.Statement;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.bankingmanagement.bankingmanagement.authentication.exception.UserAuthenticationException;
 import com.bankingmanagement.bankingmanagement.database.DatabaseConnectionDao;
 import com.bankingmanagement.bankingmanagement.database.DatabaseConnectionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.bankingmanagement.bankingmanagement.database.DatabaseConnectionException;
+import com.bankingmanagement.bankingmanagement.employeeDashboard.database.GetEmployeeDetailsDao;
+import com.bankingmanagement.bankingmanagement.employeeDashboard.database.GetEmployeeDetailsDaoImpl;
 import com.bankingmanagement.bankingmanagement.employeeDashboard.exception.EmployeeDetailsException;
 import com.bankingmanagement.bankingmanagement.employeeDashboard.service.EmployeeDetailsService;
 
-public class EmployeeDetailsServiceImpl{
-	
+public class EmployeeDetailsServiceImpl implements EmployeeDetailsService {
+
+	@Autowired
+	private DatabaseConnectionDao databaseConnectionDAO;
+
+	@Autowired
+	private GetEmployeeDetailsDao getEmployeeDetailsDao;
+
+	@Override
+	public String getEmployeeDetails(String id) throws EmployeeDetailsException {
+		try (final Connection connection = databaseConnectionDAO.getConnection();
+				final Statement statement = connection.createStatement();
+				final ResultSet empResultSet = statement.executeQuery(getEmployeeDetailsDao.getEmployeeDetailsData(id))) {
+
+			if (empResultSet == null) {
+                throw new EmployeeDetailsException("Invalid request");
+            }
+            if (empResultSet.next()) {
+                                return "";
+            }
+            else {
+                throw new EmployeeDetailsException("Invalid email and/or password");
+            }
+			
+		} catch (SQLException | DatabaseConnectionException sqlException) {
+			sqlException.printStackTrace();
+			throw new EmployeeDetailsException("Internal Error while fetching employee data");
+		}
+
+	}
 }
