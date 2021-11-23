20fa11410105f465e54060caa0fb9b7d9391baa0
Aabhaas Jain <aabhaas.jain@dal.ca>
2021-11-23 16:52:45 -0400

    code addition

--- src/main/java/com/bankingmanagement/bankingmanagement/employeeDashboard/controller/EmployeeDashboardController.java
+++ src/main/java/com/bankingmanagement/bankingmanagement/employeeDashboard/controller/EmployeeDashboardController.java
@@ -1,18 +1,25 @@
 package com.bankingmanagement.bankingmanagement.employeeDashboard.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import com.bankingmanagement.bankingmanagement.employeeDashboard.exception.EmployeeDetailsException;
+import com.bankingmanagement.bankingmanagement.employeeDashboard.service.EmployeeDetailsService;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
+@Controller
 public class EmployeeDashboardController {
 
-	
-	@RequestMapping(path= "/emp-details", method = GET)
-    public String home()
-    {
-        return "employeeDashboard";
-    }
-	
-	
+	@Autowired
+	EmployeeDetailsService employeeDetailsService;
+
+	@GetMapping(path = "/emp-dash/details")
+	public String home(Model model) throws EmployeeDetailsException {
+		System.out.println("1111111111111");
+		String empDetails=employeeDetailsService.getEmployeeDetails("111");
+		
+		return empDetails;
+	}
+
 }
--- src/main/java/com/bankingmanagement/bankingmanagement/employeeDashboard/service/impl/EmployeeDetailsServiceImpl.java
+++ src/main/java/com/bankingmanagement/bankingmanagement/employeeDashboard/service/impl/EmployeeDetailsServiceImpl.java
@@ -1,5 +1,4 @@
-package com.bankingmanagement.bankingmanagement.employeeDashboard.serviceImplementation;
-
+package com.bankingmanagement.bankingmanagement.employeeDashboard.service.impl;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -7,40 +6,39 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.bankingmanagement.bankingmanagement.database.DatabaseConnectionDao;
 import com.bankingmanagement.bankingmanagement.database.DatabaseConnectionException;
-import org.springframework.beans.factory.annotation.Autowired;
-import com.bankingmanagement.bankingmanagement.database.DatabaseConnectionException;
 import com.bankingmanagement.bankingmanagement.employeeDashboard.database.GetEmployeeDetailsDao;
-import com.bankingmanagement.bankingmanagement.employeeDashboard.database.GetEmployeeDetailsDaoImpl;
 import com.bankingmanagement.bankingmanagement.employeeDashboard.exception.EmployeeDetailsException;
 import com.bankingmanagement.bankingmanagement.employeeDashboard.service.EmployeeDetailsService;
 
+@Service
 public class EmployeeDetailsServiceImpl implements EmployeeDetailsService {
 
-	@Autowired
-	private DatabaseConnectionDao databaseConnectionDAO;
+    @Autowired
+    private DatabaseConnectionDao databaseConnectionDAO;
 
 	@Autowired
 	private GetEmployeeDetailsDao getEmployeeDetailsDao;
 
 	@Override
 	public String getEmployeeDetails(String id) throws EmployeeDetailsException {
-		try (final Connection connection = databaseConnectionDAO.getConnection();
-				final Statement statement = connection.createStatement();
-				final ResultSet empResultSet = statement.executeQuery(getEmployeeDetailsDao.getEmployeeDetailsData(id))) {
-
+		try (final Connection connection = databaseConnectionDAO.getConnection()) {
+			final Statement statement = connection.createStatement();
+			System.out.println("===================================================444444");
+			final ResultSet empResultSet = statement.executeQuery(getEmployeeDetailsDao.getEmployeeDetailsData(id));
+			System.out.println("===================================================55555");
 			if (empResultSet == null) {
-                throw new EmployeeDetailsException("Invalid request");
-            }
-            if (empResultSet.next()) {
-                                return "";
-            }
-            else {
-                throw new EmployeeDetailsException("Invalid email and/or password");
-            }
-			
+				throw new EmployeeDetailsException("Invalid request");
+			}
+			if (empResultSet.next()) {
+				return empResultSet.toString();
+			} else {
+				throw new EmployeeDetailsException("Internal Server Error");
+			}
+
 		} catch (SQLException | DatabaseConnectionException sqlException) {
 			sqlException.printStackTrace();
 			throw new EmployeeDetailsException("Internal Error while fetching employee data");
